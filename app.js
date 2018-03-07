ff2deb1c0caf27c92a03487daf373cf280c36444
Rene Tanczos <gravmatt@gmail.com>
2018-03-07 21:33:45 +0100

    update app code

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-  'use strict'
-  const http = require('http')
-  const Bot = require('messenger-bot')
+const http = require('http')
+const Bot = require('messenger-bot')
 
-  let bot = new Bot({
-    token: 'PAGE_TOKEN',
-    verify: 'VERIFY_TOKEN'
-  })
+let bot = new Bot({
+  token: 'PAGE_TOKEN',
+  verify: 'VERIFY_TOKEN',
+  app_secret: 'APP_SECRET'
+})
 
-  bot.on('error', (err) => {
-    console.log(err.message)
-  })
+bot.on('error', (err) => {
+  console.log(err.message)
+})
 
-  bot.on('message', (payload, reply) => {
-    let text = payload.message.text
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
 
-    bot.getProfile(payload.sender.id, (err, profile) => {
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
       if (err) throw err
 
-      reply({ text }, (err) => {
-        if (err) throw err
-
-        console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-      })
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
     })
   })
+})
 
 http.createServer(bot.middleware()).listen(3000)
+console.log('Echo bot server running at port 3000.')
